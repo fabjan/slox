57c67b9c409db5862c4e2c2fd8398ca5e831b044
Fabian <fabian@fmbb.se>
2022-06-04 13:31:48 +0200

    environments can nest

--- slox/src/Environment.scala
+++ slox/src/Environment.scala
@@ -1,26 +1,36 @@
 import scala.collection.mutable.HashMap
 
-class Environment() {
+class Environment(enclosing: Option[Environment] = None) {
+
   val values = new HashMap[String, LoxObject]()
 
   def define(name: String, value: LoxObject): Unit = {
     values.put(name, value)
   }
 
-  def checkDefined(name: Token): Unit = {
-    if (!values.contains(name.lexeme)) {
-      throw new RuntimeError(name, s"Undefined variable '${name.lexeme}'.")
-    }
-  }
-
   def get(name: Token): LoxObject = {
-    checkDefined(name)
-    values(name.lexeme)
+    val found = values
+      .get(name.lexeme)
+      .orElse(enclosing.map(_.get(name)))
+
+    if (found.nonEmpty) {
+      return found.get
+    }
+
+    throw new RuntimeError(name, s"Undefined variable '${name.lexeme}'.")
   }
 
   def assign(name: Token, value: LoxObject): LoxObject = {
-    checkDefined(name)
-    values.put(name.lexeme, value)
-    value
+    if (values.contains(name.lexeme)) {
+      values.put(name.lexeme, value)
+      return value
+    }
+
+    if (enclosing.nonEmpty) {
+      enclosing.foreach(_.assign(name, value))
+      return value
+    }
+
+    throw new RuntimeError(name, s"Undefined variable '${name.lexeme}'.")
   }
 }
